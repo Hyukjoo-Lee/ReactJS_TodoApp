90315735bc2d1ca4e573aec212eb498a599d8e20
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2022-07-21 19:48:57 -0700

    basic setup; import modules, set atoms, create theme...

---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+    bgColor: "whitesmoke",
+    textColor: "whitesmoke",
+    accentColor: "tomato",
+    overViewColor: "black",
+    btnColor: "black",
+}
+
+export const darkTheme: DefaultTheme = {
+    bgColor: "black",
+    textColor: "black",
+    accentColor: "#8c7ae6",
+    overViewColor: "white",
+    btnColor: "white",
+}
